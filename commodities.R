81a9eb4e23dab1a973d7541f4de84fe74e9bff78
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2021-03-04 17:59:38 +0100

    Add files via upload

--- commodities.R
+++ commodities.R
@@ -64,36 +64,55 @@ abline(v=as.numeric(commodities$Month[288]), lty = 1, lwd=1, col='gray')
 text(x=as.numeric(commodities$Month[232]),y = 6, lty = 2, lwd=1, col='black', "Emissions Trade System (2005)")
 text(x=as.numeric(commodities$Month[320]),y = 43, lty = 2, lwd=1, col='black', "Paris Agreement (2015)")
 text(x=as.numeric(commodities$Month[320]),y = 6, lty = 2, lwd=1, col='black', "Emissions Trade System (2005)")
-text(x=as.numeric(commodities$Month[202]),y = 1400, lty = 2, lwd=1, col='#ff7f7f', "2008 Global Economic Crisis", srt = 90)
+text(x=as.numeric(commodities$Month[202]),y = 1600, lty = 2, lwd=1, col='#ff7f7f', "2008 Global Economic Crisis", srt = 90)
 
-## Food -Food Index and Sugar
+## Food - Food Index and Sugar
 
-plot(commodities$Month, commodities$PriceFoodIndex, type = "l", bty = "l", pch = 1, col = "red",
+par(family= "Arial", cex = 0.6, oma = c(4, 1, 1, 1))
+
+plot(commodities$Month, commodities$PriceFoodIndex, type = "b", bty = "l", pch = 8, col = "red",
      main = "Commodity Prices in the International Market", 
      xlab = "Source: World Bank. Elaborated by Migliari, W. (2021).", 
      ylab = "US Dollars per Metric Ton/Barrel", cex = 0.3,
      ylim =c(0, 200))
-lines(commodities$Month, commodities$PriceSug, type = "l", bty = "l", pch=2, col = "darkgreen", cex = 0.3)
-abline(v=as.numeric(commodities$Month[288]), lwd=2, col='gray')
+lines(commodities$Month, commodities$PriceSug, type = "b", bty = "l", pch = 9, col = "darkgreen", cex = 0.3)
+
+legend("topright", legend = c("Food Index", "Sugar"),
+       col = c("red", "darkgreen"), 
+       pch = c(8:9),
+       bty = "n", cex = 1)
+
+abline(v=as.numeric(commodities$Month[168]), lty = 2, lwd=1, col='gray')
+abline(v=as.numeric(commodities$Month[204]), lty = 2, lwd=1, col='#ff7f7f')
+abline(v=as.numeric(commodities$Month[288]), lty = 1, lwd=1, col='gray')
+text(x=as.numeric(commodities$Month[232]),y = 6, lty = 2, lwd=1, col='black', "Emissions Trade System (2005)")
+text(x=as.numeric(commodities$Month[320]),y = 10, lty = 2, lwd=1, col='black', "Paris Agreement (2015)")
+text(x=as.numeric(commodities$Month[320]),y = 6, lty = 2, lwd=1, col='black', "Emissions Trade System (2005)")
+text(x=as.numeric(commodities$Month[202]),y = 160, lty = 2, lwd=1, col='#ff7f7f', "2008 Global Economic Crisis", srt = 90)
 
 ## Food - Beef and Poultry
 
-plot(commodities$Month, commodities$PriceBeef, type = "b", bty = "l", pch = 9, col = "red",
+par(family= "Arial", cex = 0.6, oma = c(4, 1, 1, 1))
+
+plot(commodities$Month, commodities$PriceBeef, type = "b", bty = "l", pch = 10, col = "red",
      main = "Commodity Prices in the International Market", 
      xlab = "Source: World Bank. Elaborated by Migliari, W. (2021).", 
      ylab = "US Dollars per Kg", cex = 0.3,
      ylim =c(0, 7))
-lines(commodities$Month, commodities$PricePoultry, type = "b", bty = "l", pch=10, col = "darkgreen", cex = 0.3)
-abline(v=as.numeric(commodities$Month[168]), lty = 2, lwd=1, col='gray')
-abline(v=as.numeric(commodities$Month[288]), lty = 1, lwd=1, col='gray')
+lines(commodities$Month, commodities$PricePoultry, type = "b", bty = "l", pch=11, col = "darkgreen", cex = 0.3)
 
 legend("topright", legend = c("Beef", "Poultry"),
        col = c("red", "darkgreen"), 
-       pch = c(9:10),
-       bty = "n", cex = 0.7)
+       pch = c(8:9),
+       bty = "n", cex = 1)
 
-text(2005-01-01, 1.0,  "Emissions Trade System",
-     cex=0.3, pos=3, col="black") 
+abline(v=as.numeric(commodities$Month[168]), lty = 2, lwd=1, col='gray')
+abline(v=as.numeric(commodities$Month[204]), lty = 2, lwd=1, col='#ff7f7f')
+abline(v=as.numeric(commodities$Month[288]), lty = 1, lwd=1, col='gray')
+text(x=as.numeric(commodities$Month[232]),y = 0.5, lty = 2, lwd=1, col='black', "Emissions Trade System (2005)")
+text(x=as.numeric(commodities$Month[320]),y = 0.66, lty = 2, lwd=1, col='black', "Paris Agreement (2015)")
+text(x=as.numeric(commodities$Month[320]),y = 0.5, lty = 2, lwd=1, col='black', "Emissions Trade System (2005)")
+text(x=as.numeric(commodities$Month[202]),y = 5, lty = 2, lwd=1, col='#ff7f7f', "2008 Global Economic Crisis", srt = 90)
 
 # Description: Commodity Food Price Index, 2005 = 100, includes Cereal, 
 ## Vegetable Oils, Meat, Seafood, Sugar, Bananas, and Oranges Price Indices
